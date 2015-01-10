5f10c56e36347f9dc868425180f0718eb95bcc48
Mark Feltner <mark@feltner.me>
2015-01-10 03:18:08 -0600

    feat: add css.js module

---
+++ src/css.js
@@ -0,0 +1,9 @@
+var css = {
+    vendor: [
+        require('./css/vendor/normalize.css')
+    ],
+    code: require('./css/pygments.css'),
+    main: require('./css/main.less'),
+    print: require('./css/print.css')
+};
+
